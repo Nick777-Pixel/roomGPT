1a8ac3ce817e1d60881021db6c4188d02f7dcebb
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-03-02 12:19:38 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,9 +33,9 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://twitter.com/"
           className="group"
-          aria-label="TaxPal on Twitter"
+          aria-label="Twitter"
         >
           <svg
             aria-hidden="true"
@@ -45,9 +45,9 @@ export default function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://github.com/Nutlope/roomGPT"
+          href="https://github.com/"
           className="group"
-          aria-label="TaxPal on GitHub"
+          aria-label="GitHub"
         >
           <svg
             aria-hidden="true"
