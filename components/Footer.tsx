c20318837229fb1f562e85c85ca80b82bcca51fb
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-03-02 14:10:51 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
           rel="noreferrer"
           className="font-bold hover:underline transition hover:text-gray-300 underline-offset-2"
         >
-          Replicate,{" "}
+          Replicate{" "}
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
