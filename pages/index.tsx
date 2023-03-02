d27f1a5037d0a2930c0301e651c6cca2abf0663b
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-03-02 14:06:42 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,8 @@ const Home: NextPage = () => {
           rel="noreferrer"
           className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
         >
-          Built Using{" "}
-          <span className="text-blue-600">Vercel</span>
+          Transform your space with{" "}
+          <span className="text-blue-600">AI powered design</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
